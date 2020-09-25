9bf8e8451bd7efa72122e7181f5395b6344f0c16
root <root@LAPTOP-TV1R8UF3.localdomain>
2020-09-24 20:37:54 -0700

    Added file upload capabilities.

--- app.py
+++ app.py
@@ -1,15 +1,53 @@
 """Temporary one-page web app for string quartet generator."""
-from flask import Flask, flash, redirect, request, url_for
+import os
+
+from flask import (Flask, flash, redirect, render_template, request,
+                   send_from_directory, url_for)
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'mid', 'midi'}
+UPLOAD_FOLDER = './samples/uploads/'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/index')
+def allowed_file(filename):
+    """Parse filename and make sure their extensions are allowed."""
+    return ('.' in filename and
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+            )
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """Temporary home page for string quartet web-app."""
-    return 'Hello World!'
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return redirect(url_for('uploaded_file', filename=filename))
+
+    return render_template('index.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    """Return filename."""
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == '__main__':
